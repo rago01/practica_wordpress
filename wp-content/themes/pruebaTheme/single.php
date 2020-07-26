f1b6f7b81aba3cd8368cc32c4a4d66ebde3ff205
analista <ragogarcia1@gmail.com>
2020-07-26 17:12:28 -0500

    Agregando botones para modals

--- wp-content/themes/pruebaTheme/single.php
+++ wp-content/themes/pruebaTheme/single.php
@@ -42,6 +42,26 @@
                       </div>
                     </div>
                   </div>
+
+                  <div class="modal fade" id="pasoApaso" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" >Ingredientes</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          Ingrediente 1 <br>
+                          Ingrediente 2
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
               </section>
             </div>
           </div>
