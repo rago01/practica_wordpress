bb07a54bf6a45fd39c56914ae9574a541132d522
analista <ragogarcia1@gmail.com>
2020-07-26 17:16:40 -0500

    Agregando botones para modals

--- wp-content/themes/pruebaTheme/single.php
+++ wp-content/themes/pruebaTheme/single.php
@@ -12,14 +12,19 @@
             <div class="col-6 my-3">
               <?php the_content(); ?>
               <section class="row">
-                  <div class="col-6">
+                  <div class="col-4">
                     <button type="button" data-toggle="modal" data-target="#pasoApaso" class="btn btn-danger" name="button">
                       Paso a paso
                     </button>
                   </div>
-                  <div class="col-6">
+                  <div class="col-4">
                     <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#Ingredientes" name="button">
                       Ingredientes
+                    </button>
+                  </div>
+                  <div class="col-4">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Video" name="button">
+                      Video
                     </button>
                   </div>
 
@@ -55,6 +60,27 @@
                         <div class="modal-body">
                           Ingrediente 1 <br>
                           Ingrediente 2
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="modal fade" id="Ingredientes" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" >Video</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/QqJvqMeaDtU" allowfullscreen></iframe>
+                          </div>
                         </div>
                         <div class="modal-footer">
                           <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
