cfa21fe6cc46ce0ae7e7d813889df50a9a656e7f
analista <ragogarcia1@gmail.com>
2020-07-26 17:06:44 -0500

    Cambiando tamaño de la imagen

--- wp-content/themes/pruebaTheme/single.php
+++ wp-content/themes/pruebaTheme/single.php
@@ -7,7 +7,7 @@
           <h1 class="my-3"><?php  the_title(); ?></h1>
           <div class="row">
             <div class="col-6">
-              <?php the_post_thumbnail('medium_large'); ?>
+              <?php the_post_thumbnail('medium'); ?>
             </div>
             <div class="col-6 my-3">
               <?php the_content(); ?>
