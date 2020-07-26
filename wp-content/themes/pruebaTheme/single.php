1dd9c038f9aa9254217045a3454d94d824d00cbe
analista <ragogarcia1@gmail.com>
2020-07-26 17:10:42 -0500

    Agregando botones para modals

--- wp-content/themes/pruebaTheme/single.php
+++ wp-content/themes/pruebaTheme/single.php
@@ -13,14 +13,34 @@
               <?php the_content(); ?>
               <section class="row">
                   <div class="col-6">
-                    <button type="button" class="btn btn-danger" name="button">
+                    <button type="button" data-toggle="modal" data-target="#pasoApaso" class="btn btn-danger" name="button">
                       Paso a paso
                     </button>
                   </div>
                   <div class="col-6">
-                    <button type="button" class="btn btn-warning" name="button">
+                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#Ingredientes" name="button">
                       Ingredientes
                     </button>
+                  </div>
+
+                  <div class="modal fade" id="pasoApaso" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" >Paso a paso</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          paso 1 <br>
+                          paso 2
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
               </section>
             </div>
