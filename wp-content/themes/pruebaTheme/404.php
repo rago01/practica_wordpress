c22916ee96864217848272e4bf30a2762f433d10
analista <ragogarcia1@gmail.com>
2020-07-27 11:33:40 -0500

    Agregando pagina 404

---
+++ wp-content/themes/pruebaTheme/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<main class="container">
+    <div class="pagina404 my-3">
+      <h1>404 PÁGINA NO ENCONTRADA</h1>
+      <h2>Haga click <a href="<?php echo home_url(); ?>">aquí</a> para volver </h2>
+    </div>
+</main>
+
+<?php get_footer(); ?>
